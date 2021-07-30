a50856ee5780ea91183e97aec1e8a788b5ffc068
unknown <sivaranjanib99@gmail.com>
2021-07-30 22:41:38 +0530

    [siva] Returing to the file if not exists

--- CensusAnalysisTest/UnitTest1.cs
+++ CensusAnalysisTest/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IndianStateCensusAnalyser;
+using System.Collections.Generic;
 
 namespace CensusAnalysisTest
 {
@@ -8,10 +10,10 @@ namespace CensusAnalysisTest
         [TestMethod]
         public void TestMethod1()
         {
-            string stateCensusPath = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser\ImdianStateCensusData.csv";
-            string wrongPath = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser";
-            string wrongFileType = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser";
-            string invalidDelimeter = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser";
+            string stateCensusPath = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser\IndianStateCensusData.csv";
+            string wrongPath = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser\IndiainStateCensus.csv";
+            string wrongFileType = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser\TextFile.txt";
+            string invalidDelimeter = @"C:\Users\user\source\repos\CensusAnalyser\CensusAnalyser\WrongDelimiter";
             IndianStateCensusAnalyser.CensusAdapterFactory csv = null;
             CensusAdapter adapter;
             Dictionary<string, CensusDataDAO> totalRecord;
@@ -23,6 +25,33 @@ namespace CensusAnalysisTest
                 csv = new CensusAdapterFactory();
                 adapter = new CensusAdapter();
                 totalRecord = new Dictionary<string, CensusDataDAO>();
+            }
+            //TC1.1
+            //Giving correct path it should return total count of the census list
+            [TestMethod]
+            public void GivenStateCensusReturnTotalRecord()
+            {
+                stateRecord = adapter.GetCensusData(stateCensusPath, "State,Population,AreaInSqKm,DensityPerSqKm");
+                int actual = stateRecord.Length - 1;
+                int expected = 36;
+                //assertion
+                Assert.AreEqual(actual, expected);
+            }
+            //TC 1.2
+            //Given the incorrect path return file not exist
+            [TestCategory("StateCensusAnalyser")]
+            [TestMethod]
+            public void GivenIncorrectPath()
+            {
+                try
+                {
+                    var stateRecor = adapter.GetCensusData(wrongPath, "State,Population,AreaInSqKm,DensityPerSqKm");
+
+                }
+                catch (CensusAnalyserException ce)
+                {
+                    Assert.AreEqual("File Not Found", ce.Message);
+                }
             }
 
         }
